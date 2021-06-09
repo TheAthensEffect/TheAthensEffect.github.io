678f3b43f6827aa9bac857d80776a12a41b2aadb
TheAthensEffect <85592968+TheAthensEffect@users.noreply.github.com>
2021-06-09 10:29:17 -0400

    Delete html-includes-master directory

--- html-includes-master/spec/html-includes-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import HtmlIncludesView from '../lib/html-includes-view';
-
-describe('HtmlIncludesView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
