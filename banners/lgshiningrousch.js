bb6aa0c0a55fdd135cb4efd57d842e1ac480f428
TheAthensEffect <85592968+TheAthensEffect@users.noreply.github.com>
2021-07-30 11:52:05 -0400

    Update and rename lgshiningrousch to lgshiningrousch.js

---
+++ banners/lgshiningrousch.js
@@ -0,0 +1,28 @@
+
+document.write(
+  "<article class="post">
+                  <header>
+                    <div class="title">
+                      <h2><a href="20.09.19.Shiningrousch.html">Shining a Light on Dark Matter with Dr. Douglas Clowe</h2>
+                  <body id="preview">
+                    <p class="has-line-data" data-line-start="1" data-line-end="2"><em>Clowe explains what scientists really mean when they talk about “dark matter,” provides possible theories as to what it might be, and describes his past and present research in the field to discover more about dark matter and the universe.</em></p>
+                  </body>
+                    </div>
+                    <div class="meta">
+                      <time class="published" datetime="2019-09-20">September 20, 2019</time>
+                      <a href="#" class="author"><span class="name">Katelyn Rousch</span><img src="images/fox.png" alt="" /></a>
+                    </div>
+                  </header>
+                  <span class="image featured"><img src="images/shiningrousch.png" alt="" /></span>
+                  <footer>
+                    <ul class="actions">
+                      <li><a href="20.09.19.Shiningrousch.html" class="button large">Continue Reading</a></li>
+                    </ul>
+                    <ul class="stats">
+                      <li><a href="#">General</a></li>
+                      <li><a href="#" class="icon solid fa-heart">28</a></li>
+                      <li><a href="#" class="icon solid fa-comment">128</a></li>
+                    </ul>
+                  </footer>
+                  </article>"
+  );
